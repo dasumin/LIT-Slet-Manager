91094b361f474711b5d2dd611d8d8f78f5d26400
Denis <Denis@304.ru>
2012-09-05 11:47:48 +0400

    Added updating sex and photo_url field from people.lit.msu.ru

--- update_db_from_people.php
+++ update_db_from_people.php
@@ -8,7 +8,7 @@ if (!mysql_connect($mysql_server,$mysql_user,$mysql_password))
 if (!mysql_select_db($mysql_db))
         report_error ('Не удалось подключиться к базе данных.');
 
-$tables = array ('students', 'staff', 'graduate');		
+$tables = array ('students', 'staff', 'graduate');
 
 foreach ($tables as $value) {
 	$url = "http://people.lit.msu.ru/people.php?table=".$value;
@@ -25,7 +25,7 @@ foreach ($tables as $value) {
 	if ( $xml = simplexml_load_string($response) ) {
 		for ($i=0; $i<$xml->count(); $i++) {
 			$participant = (array) $xml->person[$i];
-			
+
 			switch ($value) {
 				case 'staff':
 					$participant['litgroup'] = 'Преподаватель';
@@ -38,40 +38,41 @@ foreach ($tables as $value) {
 					if ($participant['group']==0) $participant['litgroup']='';
 					break;
 			}
-			
+
 			if (!isset($participant['sex'])) $participant['sex'] = '';
+			if (!isset($participant['photo_url'])) $participant['photo_url'] = '';
 			if ($participant['litgroup']=='') continue;
-			
+
 			$q = mysql_query ("
 				SELECT * FROM `participants`
 				WHERE `name` = '".$participant['name']."' AND
 				`surname` = '".$participant['surname']."' AND
 				`litgroup` = '".$participant['litgroup']."';
                         ");
-				
+
 			if ( mysql_num_rows ($q) == 0 ) {
 				addParticipant($participant);
 				echo 'Добавили: '.$participant['name'].' '.$participant['surname'].' ('.$participant['litgroup'].')<br>';
 			}
-                        
-                        $q_full = mysql_query ("
-				SELECT * FROM `participants`
-				WHERE `name` = '".$participant['name']."' AND
-				`surname` = '".$participant['surname']."' AND
-				`litgroup` = '".$participant['litgroup']."' AND
-                                `sex` = '".$participant['sex']."' AND
-                                `photo_url` = '".$participant['photo_url']."';
-                        ");
-                        
-                        if ( mysql_num_rows ($q) == 1 && mysql_num_rows ($q_full) == 0 ) {
-                                $f = mysql_fetch_array($q_full);
-                                $participant_new = $f['id'];
-                                $participant_new['sex'] = $participant['sex'];
-                                $participant_new['photo_url'] = $participant['photo_url'];
-                                editParticipant( $participant_new );
-				echo 'Обновили: '.$participant_new['name'].' '.$participant_new['surname'].' ('.$participant_new['litgroup'].')<br>';
+
+                        foreach ( array('sex','photo_url') as $field ) {
+
+                                $q_full = mysql_query ("
+                                        SELECT * FROM `participants`
+                                        WHERE `name` = '".$participant['name']."' AND
+                                        `surname` = '".$participant['surname']."' AND
+                                        `litgroup` = '".$participant['litgroup']."' AND
+                                        `$field` = '".$participant[$field]."';
+                                ");
+
+                                if ( mysql_num_rows ($q) == 1 && mysql_num_rows ($q_full) == 0 ) {
+                                        $f = mysql_fetch_array($q);
+                                        $participant_new = $f;
+                                        $participant_new[$field] = $participant[$field];
+                                        editParticipant( $participant_new );
+                                        echo 'Обновили: '.$participant_new['name'].' '.$participant_new['surname'].' ('.$participant_new['litgroup'].') '.$field.'<br>';
+                                }
                         }
-			
 		}
 	}
 }
