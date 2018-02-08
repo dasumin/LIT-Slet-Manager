e12a2bd4a789283e288a9ce7a1e8e0ce34f14f73
Denis Sumin <da.sumin@gmail.com>
2018-02-09 00:06:17 +0100

    Add rights to organizers

--- modules/1participantlists.php
+++ modules/1participantlists.php
@@ -17,32 +17,33 @@ $modules[$module]['title'][] = 'Информация об участнике';
 
 //$modules[$module]['groups'][] = 'guest'; // группы, которым разрешено пользоваться модулем
 $modules[$module]['groups'][] = 'admin';
-$modules[$module]['groups'][] = 'registrar';
 $modules[$module]['groups'][] = 'leader';
+$modules[$module]['groups'][] = 'organizer';
+$modules[$module]['groups'][] = 'registrar';
 
 function show_participantlists ( $action ) {
-	global $modules, $user, $participantlist, $litgroupSet;	
+	global $modules, $user, $participantlist, $litgroupSet;
 	$module = $modules['participantlists'];
 
 	switch ($action) {
 		case $module['action'][0]: // Посмотреть список участников слета
-		
-			if ( RequestModule == 'core' && check_user_access ('participantlists', $user) )  { 
+
+			if ( RequestModule == 'core' && check_user_access ('participantlists', $user) )  {
 				$field = array ( 'surname'=>'Фамилия', 'name'=>'Имя', 'litgroup'=>'Группа', 'team'=>'Звено' ); }
 			else $field = array ( 'surname'=>'Фамилия', 'name'=>'Имя', 'litgroup'=>'' );
-		
+
 			if ( empty ($_GET['sortField']) ) $sortField = 'surname'; else $sortField = $_GET['sortField'];
 			if ( empty ($_GET['sortDir']) ) $sortDir = 'ASC'; else $sortDir = $_GET['sortDir'];
 			if ( empty ($_GET['filterSurname']) ) $filterSurname = ''; else $filterSurname = $_GET['filterSurname'];
 			if ( empty ($_GET['filterGroup']) ) $filterGroup = ''; else $filterGroup = $_GET['filterGroup'];
-			
+
 			if ( isset($_GET['blacklist'])) $blacklist = '1'; else $blacklist = '0';
-			$users = formParticipantArray ( $sortField, $sortDir, $filterSurname, $filterGroup, $blacklist );			
-// Начало фильтров			
+			$users = formParticipantArray ( $sortField, $sortDir, $filterSurname, $filterGroup, $blacklist );
+// Начало фильтров
 			$alphabet = array
 				('А','Б','В','Г','Д','Е','Ж','З','И','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Э','Ю','Я');
 			$group = $litgroupSet;
-				
+
 			echo "<script>
 				function hidefilters() {
 					window.document.getElementById('filters').style.display = 'none';
@@ -54,15 +55,15 @@ function show_participantlists ( $action ) {
 					window.document.getElementById('hfilters').style.display = 'none';
 				}
 			</script>";
-				
-			echo '<div id="hfilters"><a href="javascript:showfilters()">Фильтры</a> 
+
+			echo '<div id="hfilters"><a href="javascript:showfilters()">Фильтры</a>
 			<a href="'.$_SERVER["PHP_SELF"].'?action='.$action.'" style="font-size: 75%;">Сбросить</a><span style="font-size: 75%;"> все фильтры</span></div>
 			<div id="filters">
-			<a href="javascript:hidefilters()">Фильтры</a> 
-			<a href="'.$_SERVER["PHP_SELF"].'?action='.$action.'" style="font-size: 75%;">Сбросить</a><span style="font-size: 75%;"> все фильтры</span>		
+			<a href="javascript:hidefilters()">Фильтры</a>
+			<a href="'.$_SERVER["PHP_SELF"].'?action='.$action.'" style="font-size: 75%;">Сбросить</a><span style="font-size: 75%;"> все фильтры</span>
 			<div>
 			<p>Фамилия:<br />';
-				
+
 			for ($i=0; $i<count($alphabet); $i++)
 			echo '
 				<a href="'.$_SERVER["PHP_SELF"].'?action='.$action.'&sortField='.$sortField.'
@@ -70,12 +71,12 @@ function show_participantlists ( $action ) {
 			echo '
 			</p><p>
 			Группа:<br />';
-			
+
 			for ($i=0; $i<count($group); $i++) {
 			echo '
 				<a href="'.$_SERVER["PHP_SELF"].'?action='.$action.'&sortField='.$sortField.'
 				&sortDir='.$sortDir.'&filterSurname='.$filterSurname.'&filterGroup='.$group[$i].'">'.$group[$i].'</a> ';
-				
+
 			if ( !empty ($group[$i+1]) && ( ( $group[$i][0] != '1' && $group[$i][0] != $group[$i+1][0] ) || ( $group[$i][1] != $group[$i+1][1] ) ) ) echo '<br />';
 			}
 
@@ -98,21 +99,21 @@ function show_participantlists ( $action ) {
 					location.href = "?action=participant_info&id="+id+"&fromlist";
 				}
 			</script>
-			
+
 			<table id="participanttable">
 				<tr>';
 			if ( RequestModule == 'participantlist' ) echo '
 					<th></th>
 				';
-				
+
 			foreach ($field as $f=>$text) {
 				echo '
-					<th>'.$text.' 
+					<th>'.$text.'
 					<a title="По возрастанию" href="'.$_SERVER["PHP_SELF"].'?action='.$action.'&sortField='.$f.'&sortDir=ASC&filterSurname='.$filterSurname.'&filterGroup='.$filterGroup.'" class="sort-arrow">&uarr;</a><a title="По убыванию" href="'.$_SERVER["PHP_SELF"].'?action='.$action.'&sortField='.$f.'&sortDir=DESC&filterSurname='.$filterSurname.'&filterGroup='.$filterGroup.'" class="sort-arrow">&darr;</a></th>
 			';
 			}
 			echo '</tr>';
-			
+
 			if ( count ($users) == 0 ) echo '<tr><td colspan="'.count ($field).'">Ничего не найдено</td></tr>';
 			foreach ($users as $key=>$user) {
 				echo '<tr style="cursor:pointer" id="'.$user['id'].'" onmouseover="highlight(\''.$user['id'].'\')" onmouseout="nohighlight(\''.$user['id'].'\')">';
@@ -121,9 +122,9 @@ function show_participantlists ( $action ) {
 				';
 				foreach ($field as $f=>$text) {
 					if ( $f=='litgroup' || $f=='team' ) $align='right'; elseif ( $f=='id' ) $align='center'; else $align='left';
-					
+
 					if ($f == 'litgroup' && $user[$f] == 'Преподаватель') echo '<td align="'.$align.'">П</td>'."\n";
-					 elseif ($f == 'litgroup' && $user[$f] == 'Выпускник') echo '<td align="'.$align.'">В</td>'."\n";					
+					 elseif ($f == 'litgroup' && $user[$f] == 'Выпускник') echo '<td align="'.$align.'">В</td>'."\n";
 					  else {
 						echo '<td align="'.$align.'" onclick="viewInfo('.$user['id'].')">';
 						if ($f == 'litgroup' && $user['blacklist'] == 1) {
@@ -138,40 +139,40 @@ function show_participantlists ( $action ) {
 			echo '</table>';
 // Конец вывода таблицы
 			break;
-			
-			
+
+
 		// Информация об участнике
 		case 'participant_info':
 			$participantlist = TRUE;
-			
+
 			echo '
 			<script type="text/javascript">
 				function participant(id) {
 					document.getElementById(\'id\').value=id;
-				}			
-			</script>	
+				}
+			</script>
 			';
-			
+
 			if ( ( isset ($_POST['id']) && $id = $_POST['id'] ) || ( isset ($_GET['id']) && $id = $_GET['id']  ) ) {
 				if ( isset ($_GET['fromlist']) ) echo '<p><a href="javascript:history.back()">Вернуться к таблице</a></p>';
 				echo '<p>';
 				print_participant_info ( $id ) ;
 				if (check_user_access('manage_participants')) echo '<p style="text-align:center;"><a href="?action=edit_participant&id='.$id.'"><i>изменить информацию</i></a></p>'."\n";
-				echo '</p>';				
+				echo '</p>';
 				echo '<p style="clear: both;"></p><p style="clear: both; margin: 40px 0 0 0;"><i>Другой участник</i></p>';
 			}
-			
+
 			echo '
 			<form method="post" action="'.$_SERVER["PHP_SELF"].'?action='.$action.'">
-			<p style="margin-top: 0">Введите id участника:<br />			
+			<p style="margin-top: 0">Введите id участника:<br />
 				<input type="text" name="id" id="id" />
 				<input type="submit" name="'.$action.'" value="Вывести информацию">
 			</p>
 			</form>
 			';
 			break;
-			
-		
+
+
 	}
 }
 ?>
